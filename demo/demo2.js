99351d5465873b88512943d022508fae0554fc0a
godsong <gaosong@hitour.cc>
2015-12-24 17:45:06 +0800

    update

--- demo/demo2.js
+++ demo/demo2.js
@@ -2,24 +2,37 @@
  * Created by godsong on 15-12-18.
  */
 
-var Http=require('http');
-var Url=require('url');
-var ReadyStream=require('../ReadyStream.js');
+var Http = require('http');
+var Url = require('url');
+var ReadyStream = require('../ReadyStream.js');
 //HttpWriteRequest类
 // 名字随意取，其实更建议易懂的名字，比如HttpData。
 // 只要包含doWrite方法即可
 
-var HttpData=ReadyStream.WriteRequest.implement({
-    constructor:function HttpData(url){
-        this.url=url;
+var HttpData = ReadyStream.WriteRequest.implement({
+    constructor : function HttpData(url) {
+        if(typeof url === 'object') {
+            this.urlObj = url;
+        }
+        else {
+            this.urlObj = Url.parse(url);
+        }
     },
-    doWrite:function(readyStream){
-        var urlObj = Url.parse(this.url);
+    resolveRedirectUrl:function(headers){
+        var redirectUrlObj = Url.parse(headers['Location'] || headers['location']);
+        redirectUrlObj.protocol = redirectUrlObj.protocol || this.urlObj.protocol;
+        redirectUrlObj.host = redirectUrlObj.host || this.urlObj.host;
+        redirectUrlObj.hostname = redirectUrlObj.hostname || this.urlObj.hostname;
+        redirectUrlObj.port = redirectUrlObj.port || this.urlObj.port;
+        return redirectUrlObj;
+    },
+    doWrite     : function(readyStream) {
+        var self=this;
         var request = Http.request({
-            host:urlObj.hostname,
-            method  : 'get',
-            path    : urlObj.path,
-            port    : urlObj.port || 80
+            host   : this.urlObj.hostname,
+            method : 'get',
+            path   : this.urlObj.path,
+            port   : this.urlObj.port || 80
         }, function(res) {
             res.on('data', function(chunk) {
                 //请求到数据之后写到readyStream里
@@ -33,14 +46,19 @@ var HttpData=ReadyStream.WriteRequest.implement({
                     //所以必须在你认为异步结束的时候执行.end
                     readyStream.end();
                 }
+                else if(res.statusCode == 302 || res.statusCode == 301) {
+                    var urlObj=self.resolveRedirectUrl(res.headers);
+                    readyStream.put(new HttpData(urlObj));
+                    readyStream.end();
+                }
             });
         });
         request.end();
     }
 });
 //实现doWrite方法
-var stream=new ReadyStream();
+var stream = new ReadyStream();
 stream.put(new HttpData('http://www.jd.com/robots.txt'));
-stream.put("end");
+//stream.put(new HttpData('http://localhost:60001/test/redirect'));
 stream.writeFile('./in2.txt');
 stream.pipe(process.stdout);
